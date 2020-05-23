f8d5e19e64798e0d9cf496951cbf078c51abb26c
mananjhaveri <54372210+mananjhaveri@users.noreply.github.com>
2020-05-23 23:22:43 +0530

    Update model.py

--- model.py
+++ model.py
@@ -42,10 +42,28 @@ def make_df():
     scaler = Normalizer()
     
     
-    pipeline = make_pipeline(tfidf, model, scaler)
+    pipeline = make_pipeline(tfidf, model)
     
     df_transformed = pd.DataFrame(pipeline.fit_transform(df.transformed_summary), index = df.titles)
-    return df_transformed
-
+    
+    genre_master = ["drama", "comedy", "romance", "horror", "action", "sci-fi", "sport", "fantasy", "crime", "music", "war", "biography", "thriller", "mystery", "family", "animation", "adventure", "musical", "history", "western", "film-noir" ]
+    genre_dict = {"drama": 1, "comedy": 2, "romance": 3, "horror": 4, "action": 5, "sci-fi": 6, "sport":7, "fantasy": 8, "crime":  9, "music": 10, "war": 11, "biography": 12, "thriller": 13, "mystery": 14, "family": 15, "animation": 16, "adventure": 17, "musical": 18, "history": 19, "western": 20, "film-noir": 21}
+    full = []
+    for g in df.genre:
+      g = g.lower()
+      ind = [genre_dict.get(x) for x in g.split(", ")]
+      temp = []
+      for i in range(1, len(genre_master) + 1):
+        if i in ind:
+          temp.append(1)
+        else:
+          temp.append(0)
+      full.append(temp)
+      
+      df_genre = pd.DataFrame(full, columns = genre_master, index = df.titles)
+      df_combined = pd.DataFrame(scaler.fit_transform(pd.concat([df_transformed, df_genre], axis = 1)), index = df.titles)
+    
+    
+    return df_combined
 df = make_df()
 df.to_excel("df_transformed.xlsx")
